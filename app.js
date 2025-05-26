8bc7b01e7655aa9e9e47427cc5035988972ffc99
PROF ROMULO <rjlpena@gmail.com>
2025-05-26 15:08:27 +0000

    ok

---
+++ app.js
@@ -0,0 +1,6 @@
+function criaCartao(categoria, pergunta, resposta) {
+    let container = document.getElementById('container')
+    let cartao = document.createElement('article')
+    cartao.className = 'cartao';
+
+}
