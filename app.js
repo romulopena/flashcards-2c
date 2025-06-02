33eedcd452ab7f220a2e40ef6d3f65febbe0dc15
PROF ROMULO <rjlpena@gmail.com>
2025-06-02 14:26:38 +0000

    ok

--- app.js
+++ app.js
@@ -2,5 +2,18 @@ function criaCartao(categoria, pergunta, resposta) {
     let container = document.getElementById('container')
     let cartao = document.createElement('article')
     cartao.className = 'cartao'
-
+    cartao.innerHTML = `
+    <article class="cartao">
+                <div class="cartao__conteudo">
+                    <h3>Programação</h3>
+                    <div class="cartao__conteudo__pergunta">
+                        <p>O que é JS?</p>
+                    </div>
+                    <div class="cartao__conteudo__resposta">
+                        <p>JS é uma linguagem de programação.</p>
+                    </div>
+                </div>
+            </article>
+            `
+            container.appendChild(cartao)
 }
